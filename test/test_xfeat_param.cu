fc07ebae9d2eec39770186b842f7e09b1d20d0e5
Manoj Velmurugan <v.manoj1996@gmail.com>
2025-07-11 23:21:40 -0400

    test xfeat param done

--- test/test_xfeat_param.cu
+++ test/test_xfeat_param.cu
@@ -15,9 +15,25 @@ int main() {
     std::cout << "Loaded " << params.numParams() << " parameters" << std::endl;
     params.printParams();
     
-    auto weights = params.makeParam4D("net.block4.1.layer.0.weight");
-    std::cout << "N D tensor first value: " << weights(0, 0, 0, 0) << std::endl;
+    {
+        auto weights = params.makeParam0D("net.block_fusion.1.layer.1.num_batches_tracked");
+        std::cout << "0 D tensor first value: " << weights() << std::endl;
+    }
+
+    {
+        auto weights = params.makeParam1D("net.fine_matcher.1.running_var");
+        std::cout << "1 D tensor first value: " << weights(0) << std::endl;
+    }
     
+    {
+        auto weights = params.makeParam2D("net.fine_matcher.6.weight");
+        std::cout << "2 D tensor first value: " << weights(0, 0) << std::endl;
+    }
+    
+    {
+        auto weights = params.makeParam4D("net.block4.1.layer.0.weight");
+        std::cout << "4 D tensor first value: " << weights(0, 0, 0, 0) << std::endl;
+    }
     
     std::cout << "Test completed!" << std::endl;
     return 0;
