424088b79218f6e8702ba8c936f0f245c3495ef7
Manoj Velmurugan <v.manoj1996@gmail.com>
2025-07-21 18:01:59 -0400

    sobel also worked fine.

--- matlab/test_conv2d.m
+++ matlab/test_conv2d.m
@@ -3,33 +3,39 @@ clear mex
 clear classes
 clear all
 
-% Apply a gassian blur on image using conv kernel
-
 height = 200
 width  = 400
- 
-% [ci, height, width]
-input = single(randn(1, height, width));  
+input_prop = clib.xfeat.ImgProperty(height, width)  % height, width
 
+input_image = rgb2gray(imread('peppers.png'));
+input_image = imresize(input_image, [height, width]);
+input_device = host2device(input_image);
+
+%% Apply a gaussian blur on image using conv kernel
 % [co, ci, k1, k2] 
 kernel = single(ones(1, 1, 3, 3));
 kernel = kernel / sum(abs(kernel(:)));
-
-% Create structures for your C++ class
-
-input_prop = clib.xfeat.ImgProperty(height, width)  % height, width
 params     = clib.xfeat.Conv2DParams(3, 3, 1, 1, 1, 1, 1, 1)  % k1,k2,ci,co,s1,s2,p1,p2
 
-conv = clib.xfeat.Conv2D(input_prop, params);
+blur = clib.xfeat.Conv2D(input_prop, params);
 kernel_clib = flatten(kernel);
+blur.set_kernel(kernel_clib);
 
-conv.set_kernel(kernel_clib);
-
-% Input image processing
-input_image = rgb2gray(imread('peppers.png'));
-input_image = imresize(input_image, [height, width]);
-
-input_device = host2device(input_image);
-output_device = conv.forward(input_device);
+output_device = blur.forward(input_device);
 output_host = device2host(output_device);
 
+
+%% Apply Sobel edge detection horizontally
+sobel_x = single([-1, 0, 1; 
+                 -2, 0, 2; 
+                 -1, 0, 1]);
+
+sobel_filter = clib.xfeat.Conv2D(input_prop, params);
+
+kernel_clib = flatten(sobel_x);
+sobel_filter.set_kernel(kernel_clib);
+
+output_device = sobel_filter.forward(input_device);
+sobel_output = device2host(output_device);
+
+% sobel also worked
