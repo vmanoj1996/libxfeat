036488c20125c51f46a8535619a990ad0f1cec5b
Manoj Velmurugan <v.manoj1996@gmail.com>
2025-07-26 00:18:52 -0400

    refac

--- src/xfeat.cpp
+++ src/xfeat.cpp
@@ -3,10 +3,12 @@
 #include "fold.hpp"
 #include <opencv2/opencv.hpp>
 
-
-XFeat::XFeat(std::string model_file, int height_, int width_): model(model_file), height(height_), width(width_)
+XFeat::XFeat(std::string model_file, int height_, int width_) : model(model_file), height(height_), width(width_)
 {
-    /* Keypoint head - separate branch using fold/unfold
+    using std::to_string;
+
+    {
+        /* Keypoint head - separate branch using fold/unfold
             (keypoint_head): Sequential(
             (0): BasicLayer(
                 (layer): Sequential(
@@ -30,70 +32,59 @@ XFeat::XFeat(std::string model_file, int height_, int width_): model(model_file)
                 )
             )
                 (3): Conv2d(64, 65, kernel_size=(1, 1), stride=(1, 1))
-    */
+        */
+        kp_layers.emplace_back(make_fold(height, width));
 
-    using std::to_string;
+        auto [cheight, cwidth] = std::make_pair(height / 8, width / 8);
+        const int KP_CH = 64;
 
-    kp_layers.emplace_back(make_fold(height, width));
+        // 3 conv layers with BatchNorm+ReLU
+        for (int i = 0; i < 3; i++)
+        {
+            auto layername = "net.keypoint_head." + to_string(i) + ".layer.";
+            kp_layers.emplace_back(
+                conv2d(
+                    {KP_CH, cheight, cwidth},
+                    {1, 1, KP_CH, KP_CH, 1, 1, 0, 0},
+                    model.getParam(layername + "0.weight"),
+                    BNR(model.getParam(layername + "1.running_mean"), model.getParam(layername + "1.running_var"))));
+        }
 
-    int cheight = height/8;
-    int cwidth  = width/8;
+        // Final conv layer
+        kp_layers.emplace_back(
+            conv2d(
+                {KP_CH, cheight, cwidth},
+                {1, 1, KP_CH, KP_CH + 1, 1, 1, 0, 0},
+                model.getParam("net.keypoint_head.3.weight")));
 
-    std::cout<<"Added fold layer\n";
-
-    const int KP_CHANNEL = 64;
-
-    for(int i=0; i<3; i++)
-    {
-        ImgProperty img_property(KP_CHANNEL, cheight, cwidth);
-        auto weight = model.getParam("net.keypoint_head." + to_string(i) + ".layer.0.weight");
-        Conv2DParams params(1, 1, KP_CHANNEL, KP_CHANNEL, 1, 1, 0, 0);
-
-        auto mean = model.getParam("net.keypoint_head." + to_string(i) + ".layer.1.running_mean");
-        auto vars = model.getParam("net.keypoint_head." + to_string(i) + ".layer.1.running_var");
-        
-        kp_layers.emplace_back(conv2d(img_property, params, weight, BNR(mean, vars)));
-
-        std::cout<<"Added conv2d layer\n";
-
+        kp_layers.emplace_back(make_unfold(height, width));
     }
-
-    auto img_property = ImgProperty(KP_CHANNEL, cheight, cwidth);
-    auto weight = model.getParam("net.keypoint_head.3.weight");
-    Conv2DParams params(1, 1, KP_CHANNEL, KP_CHANNEL+1, 1, 1, 0, 0);
-
-    kp_layers.emplace_back(conv2d(img_property, params, weight)); std::cout<<"Added conv2d layer\n";
-
-    kp_layers.emplace_back(make_unfold(height, width));     std::cout<<"Added unfold layer\n";
-
 }
 
-DevicePointer<FLOAT>& XFeat::forward(DevicePointer<FLOAT>& input)
+DevicePointer<FLOAT> &XFeat::forward(DevicePointer<FLOAT> &input)
 {
-    auto* previous_output = &input;
-    
+    auto *previous_output = &input;
+
     // run through the keypoint layers
     int count = 0;
-    for(auto& layer: kp_layers)
+    for (auto &layer : kp_layers)
     {
-        std::cout<<"Inference: KP: Layer "<<count++<<"\n";
+        std::cout << "Inference: KP: Layer " << count++ << "\n";
         // although I am taking the const, trust Manoj lol
-        auto* output = const_cast<DevicePointer<float>*>( &(layer->forward(*previous_output)) );
+        auto *output = const_cast<DevicePointer<float> *>(&(layer->forward(*previous_output)));
         previous_output = output;
     }
 
     return *previous_output;
 }
 
-
 int main()
 {
     cv::Mat img = cv::imread("../data/TajMahal.png", cv::IMREAD_GRAYSCALE);
-    cv::Mat resized_img; cv::resize(img, resized_img, cv::Size(64, 32));
+    // cv::Mat resized_img; cv::resize(img, resized_img, cv::Size(64, 32));
 
     cv::Mat img_float;
-    resized_img.convertTo(img_float, CV_32F, 1.0/255.0);
-
+    img.convertTo(img_float, CV_32F, 1.0 / 255.0);
 
     std::vector<float> img_vec(img_float.begin<float>(), img_float.end<float>());
     std::vector<int> dims = {img.rows, img.cols};
@@ -103,8 +94,6 @@ int main()
     XFeat feat("../params/xfeat_weights.h5", img.rows, img.cols);
 
     feat.forward(img_device);
-    
 
-    std::cout<<"Reached the end of main\n";
-
+    std::cout << "Reached the end of main\n";
 }
