3e7a982b0ede1295dcae39cb8ccefe77d75e26cd
Manoj Velmurugan <v.manoj1996@gmail.com>
2025-07-28 02:28:16 -0400

    test modifcations. to do: check the logic, unit test the new blocks, make keypoints work agian and proceed to the rest

--- src/xfeat.cpp
+++ src/xfeat.cpp
@@ -288,12 +288,12 @@ int main()
 
     auto [heatmap, keypoints, feats] = feat.forward(img_device);
 
-    // std::vector<float> heatmap_vec = heatmap.get_value();
-    // std::vector<int> heatmap_shape = heatmap.get_shape();
+    std::vector<float> heatmap_vec = heatmap.get_value();
+    std::vector<int> heatmap_shape = heatmap.get_shape();
 
-    // cv::Mat heatmap_mat(heatmap_shape[0], heatmap_shape[1], CV_32F, heatmap_vec.data());
-    // cv::imshow("Heatmap", heatmap_mat);
-    // cv::waitKey(0);
+    cv::Mat heatmap_mat(heatmap_shape[1], heatmap_shape[2], CV_32F, heatmap_vec.data());
+    cv::imshow("Heatmap", heatmap_mat);
+    cv::waitKey(0);
 
     std::cout << "Reached the end of main\n";
 }
